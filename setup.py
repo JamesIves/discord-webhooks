b08f2f021da58f7302152bfb12ad436f27c67521
James Ives <iam@jamesiv.es>
2018-11-09 09:55:17 -0500

    Adjusting Setup for PyPi

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+long_description = open('README.md').read()
 
 setup(
   name='Discord Webhooks',
-  version='1.0.0',
-  packages=find_packages(exclude=['tests', 'tests.*'])
+  version='1.0.1',
+  packages=find_packages(exclude=['tests', 'tests.*']),
   url='https://github.com/JamesIves/discord-webhooks',
   author='James Ives',
   author_email='iam@jamesiv.es',
   description='Easy to use package for Python which allows for sending of webhooks to a Discord server.',
-  long_description='Easy to use package for Python which allows for sending of webhooks to a Discord server.',
+  long_description=long_description,
   license='MIT',
   install_requires=[
     'requests==2.20.0'
